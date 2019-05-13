9a1129c0468cde708f2fcb1a7d931fd0c580ee29
terminalforlife <terminalforlife@yahoo.com>
2019-05-13 01:28:32 +0100

    Updated update_links.sh

--- update_links.sh
+++ update_links.sh
@@ -1,51 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #----------------------------------------------------------------------------------
-# Project Name      - i3config/update_links.sh
+# Project Name      - i3Config/update_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Thu  9 May 13:44:39 BST 2019
+# Last Change       - Mon 13 May 01:28:23 BST 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #------------------------------------------------------------------------------MAIN
 
-XERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; exit 1; }
-ERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; }
+FAIL(){
+	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
+	[ $1 -eq 1 ] && exit 1
+}
 
 declare -i DEPCOUNT=0
-for DEP in /bin/{ln,rm}; {
-	[ -x "$DEP" ] || {
-		ERR "$LINENO" "Dependency '$DEP' not met."
+for DEP in ln rm; {
+	if ! type -fP "$DEP" > /dev/null 2>&1; then
+		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
 		DEPCOUNT+=1
-	}
+	fi
 }
 
 [ $DEPCOUNT -eq 0 ] || exit 1
 
-[ "${PWD//*\/}" == "i3Config" ] || {
-	XERR "$LINENO" "Not in the repository's root directory."
-}
+if ! [ "${PWD//*\/}" == "i3Config" ]; then
+	FAIL 1 "$LINENO" "Not in the repository's root directory."
+fi
 
 for FILE in .i3a/* ; {
-	/bin/rm -v "$HOME/$FILE" 2>&-
-	/bin/ln -v "$FILE" "$HOME/$FILE" 2>&-
+	rm -v "$HOME/$FILE" 2>&-
+	ln -v "$FILE" "$HOME/$FILE" 2>&-
 }
 
 CFILE="$HOME/.config/i3/config"
-/bin/rm -v "$CFILE" 2>&-
-/bin/ln -v config "$CFILE" 2>&-
+rm -v "$CFILE" 2>&-
+ln -v config "$CFILE" 2>&-
 
 CFILE="$HOME/.i3bbelow"
-/bin/rm -v "$CFILE" 2>&-
-/bin/ln -v .i3bbelow "$CFILE" 2>&-
+rm -v "$CFILE" 2>&-
+ln -v .i3bbelow "$CFILE" 2>&-
 
 CFILE="$HOME/.i3babove"
-/bin/rm -v "$CFILE" 2>&-
-/bin/ln -v .i3babove "$CFILE" 2>&-
+rm -v "$CFILE" 2>&-
+ln -v .i3babove "$CFILE" 2>&-
 
 CFILE="$HOME/.libi3bview"
-/bin/rm -v "$CFILE" 2>&-
-/bin/ln -v .libi3bview "$CFILE" 2>&-
+rm -v "$CFILE" 2>&-
+ln -v .libi3bview "$CFILE" 2>&-
 
 # vim: noexpandtab colorcolumn=84 tabstop=8 noswapfile nobackup
