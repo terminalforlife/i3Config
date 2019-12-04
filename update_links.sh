b01584876bfa9e2dc6fecf945276604780e329a4
terminalforlife <terminalforlife@yahoo.com>
2019-12-04 22:39:59 +0000

    Convert link updater to POSIX Bourne

--- update_links.sh
+++ update_links.sh
@@ -1,52 +1,53 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #----------------------------------------------------------------------------------
 # Project Name      - i3Config/update_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Mon 13 May 01:28:23 BST 2019
+# Last Change       - Wed  4 Dec 22:39:21 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #------------------------------------------------------------------------------MAIN
 
-FAIL(){
-	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
-	[ $1 -eq 1 ] && exit 1
+Err(){
+	printf "ERROR: %s\n" "$2" 1>&2
+	[ $1 -gt 1 ] && exit $1
 }
 
-declare -i DEPCOUNT=0
-for DEP in ln rm; {
-	if ! type -fP "$DEP" > /dev/null 2>&1; then
-		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
-		DEPCOUNT+=1
+DepCount=0
+for CurDep in ln rm; do
+	if ! command -v "$CurDep" 1> /dev/null 2>&1; then
+		Err 0 "Dependency '$CurDep' not met."
+		DepCount=$((DepCount + 1))
 	fi
-}
+done
 
-[ $DEPCOUNT -eq 0 ] || exit 1
+[ $DepCount -eq 0 ] || exit 1
 
-if ! [ "${PWD//*\/}" == "i3Config" ]; then
-	FAIL 1 "$LINENO" "Not in the repository's root directory."
+if ! [ "${PWD##*/}" = 'i3Config' ]; then
+	Err 1 "Not in the repository's root directory."
 fi
 
-for FILE in .i3a/* ; {
-	rm -v "$HOME/$FILE" 2> /dev/null
-	ln -v "$FILE" "$HOME/$FILE" 2> /dev/null
-}
+{
+	for FILE in .i3a/*; do
+		rm -v "$HOME/$FILE"
+		ln -v "$FILE" "$HOME/$FILE"
+	done
 
-CFILE="$HOME/.config/i3/config"
-rm -v "$CFILE" 2> /dev/null
-ln -v config "$CFILE" 2> /dev/null
+	CFILE="$HOME/.config/i3/config"
+	rm -v "$CFILE"
+	ln -v config "$CFILE"
 
-CFILE="$HOME/.i3bbelow"
-rm -v "$CFILE" 2> /dev/null
-ln -v .i3bbelow "$CFILE" 2> /dev/null
+	CFILE="$HOME/.i3bbelow"
+	rm -v "$CFILE"
+	ln -v .i3bbelow "$CFILE"
 
-CFILE="$HOME/.i3babove"
-rm -v "$CFILE" 2> /dev/null
-ln -v .i3babove "$CFILE" 2> /dev/null
+	CFILE="$HOME/.i3babove"
+	rm -v "$CFILE"
+	ln -v .i3babove "$CFILE"
 
-CFILE="$HOME/.libi3bview"
-rm -v "$CFILE" 2> /dev/null
-ln -v .libi3bview "$CFILE" 2> /dev/null
-
+	CFILE="$HOME/.libi3bview"
+	rm -v "$CFILE"
+	ln -v .libi3bview "$CFILE"
+} 2> /dev/null
