e712db6d45d611fafcaedd4ce45868af3415915b
terminalforlife <terminalforlife@yahoo.com>
2019-12-06 17:01:33 +0000

    Correct file paths

--- devutils/links.sh
+++ devutils/links.sh
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------------
 # Project Name      - i3Config/devutils/links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Fri  6 Dec 15:57:21 GMT 2019
+# Last Change       - Fri  6 Dec 17:00:56 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
@@ -17,12 +17,12 @@ ChkDep ln rm
 cd "$HOME/GitHub/terminalforlife/Personal/i3Config"
 
 {
-	CFILE="$HOME/.config/i3/config"
-	rm -v "$CFILE"
-	ln -v source/config "$CFILE"
+	rm -v "$HOME/.config/i3/config"
+	ln -v source/config "$HOME/.config/i3/config"
 
-	for FILE in source/.i3a/*; do
-		rm -v "$HOME/$FILE"
-		ln -v "$FILE" "$HOME/$FILE"
+	cd source/.i3a
+	for CurFile in *; do
+		rm -v "$HOME/.i3a/$CurFile"
+		ln -v "$CurFile" "$HOME/.i3a/$CurFile"
 	done
 } 2> /dev/null
