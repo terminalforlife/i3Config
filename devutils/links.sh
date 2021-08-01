bf904b08630dd7dc647cb110f763129be72fd9b8
terminalforlife <terminalforlife@yahoo.com>
2021-08-01 21:00:37 +0100

    Improve links.sh

--- devutils/links.sh
+++ devutils/links.sh
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Project Name      - i3Config/devutils/links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Wed 11 Nov 16:42:25 GMT 2020
+# Last Change       - Sun  1 Aug 21:00:08 BST 2021
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
@@ -14,18 +14,16 @@
 #   coreutils (>= 8.25-2)
 #------------------------------------------------------------------------------
 
-cd "$HOME/GitHub/terminalforlife/Personal/i3Config"
+exec 2> /dev/null
 
-{
-	rm -v "$HOME/.config/i3/config"
-	ln -v source/config "$HOME/.config/i3/config"
+if cd "$HOME/GitHub/terminalforlife/Personal/i3Config"; then
+	mkdir -pv "$HOME/.i3a"
 
-	[ -d "$HOME/.i3a" ] || mkdir "$HOME/.i3a"
+	ln -vf source/config "$HOME/.config/i3/config"
 
 	if cd source/.i3a; then
 		for CurFile in *; do
-			rm -v "$HOME/.i3a/$CurFile"
-			ln -v "$CurFile" "$HOME/.i3a/$CurFile"
+			ln -vf "$CurFile" "$HOME/.i3a/$CurFile"
 		done
 	fi
-} 2> /dev/null
+fi
